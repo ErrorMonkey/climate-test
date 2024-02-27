c562b9cdd4b44112ac33892a665925209b9daee0
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-02-27 17:38:02 +0900

    Feat: test 기능 간단하게 구현

---
+++ src/app/assets/questions.tsx
@@ -0,0 +1,41 @@
+const questions = [
+  {
+    questionText: '기후변화에 대한 정부의 대응책을 높이 평가하는 국가는?',
+    answerOptions: [
+      { answerText: '베네수엘라', isCorrect: false },
+      { answerText: '대한민국', isCorrect: false },
+      { answerText: '일본', isCorrect: true },
+      { answerText: '러시아', isCorrect: true },
+    ],
+  },
+  {
+    questionText: '온실가스 배출량이 가장 많은 산업은?',
+    answerOptions: [
+      { answerText: '임목업', isCorrect: false },
+      { answerText: '해운 산업', isCorrect: false },
+      { answerText: '철강 산업', isCorrect: true },
+      { answerText: '자동차', isCorrect: true },
+    ],
+  },
+  {
+    questionText: '온실가스 배출량이 가장 많은 국가는?',
+    answerOptions: [
+      { answerText: '소의 메탄 가스', isCorrect: false },
+      { answerText: '해운 산업', isCorrect: false },
+      { answerText: '철강 산업', isCorrect: true },
+      { answerText: '자동차', isCorrect: true },
+    ],
+  },
+  {
+    questionText:
+      '원자력 발전소의 가동 중단으로 온실가스 배출량이 가장 적은 국가는?',
+    answerOptions: [
+      { answerText: '없음', isCorrect: false },
+      { answerText: '독일', isCorrect: false },
+      { answerText: '프랑스', isCorrect: true },
+      { answerText: '자동차', isCorrect: true },
+    ],
+  },
+];
+
+export default questions;
